d20d2a2566a5b7b3dcbb3a0d7054e2f48834bb1c
Nico <5westbury5@gmail.com>
2019-01-05 22:22:07 -0500

    Add prod build files

---
+++ quad/client/build/precache-manifest.e357e026d36f904c9bf3916081709d0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ba60b7eb05dd0b0e5bc5",
+    "url": "/static/js/main.ba60b7eb.chunk.js"
+  },
+  {
+    "revision": "b61bf84cb80496ce0597",
+    "url": "/static/js/1.b61bf84c.chunk.js"
+  },
+  {
+    "revision": "ba60b7eb05dd0b0e5bc5",
+    "url": "/static/css/main.dd7130b9.chunk.css"
+  },
+  {
+    "revision": "4f0bbbc4efa303a34a4fca22613f1804",
+    "url": "/index.html"
+  }
+];
